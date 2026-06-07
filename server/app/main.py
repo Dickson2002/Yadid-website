7364b5676f11d3860c27940861d845b71b065cf9
Muhanguzi Muhwezi <michealphenh10@gmail.com>
2026-06-07 20:18:57 +0300

    Remove localhost from CORS origins

--- server/app/main.py
+++ server/app/main.py
@@ -26,9 +26,6 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:4173",
-        "http://127.0.0.1:5173",
         "https://jedidahmunyaka.com",
         "https://www.jedidahmunyaka.com",
     ],
